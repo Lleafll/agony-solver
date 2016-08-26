236cd07e050632c9fb88b254bf4e0352668e2ed0
Lars Lorenz <lars.ll.lorenz@gmail.com>
2016-08-26 16:28:05 +0200

    Add more iteration wrappers

--- Increment.py
+++ Increment.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+#=======================================
 # Modules
+#=======================================
 from math import sqrt
 import numpy as np
 import itertools
@@ -8,8 +10,10 @@ import os.path as path
 from random import randint
 from random import uniform
 
+#=======================================
 # Constants
-ITERATIONS = 1000000
+#=======================================
+ITERATIONS = 10000
 INCREMENT_MIN = 0
 INCREMENT_MAX = 0.32
 MAX_TARGETS = 5
@@ -17,7 +21,9 @@ MAX_TICKS = 10
 FILE_NAME = u"%i_%i_results.npy" % (MAX_TARGETS, MAX_TICKS)
 IMPORT_FROM_FILE = True
 
+#=======================================
 # Load or initialize
+#=======================================
 global iteratedTicks
 def initialize_iteratedTicks():
   global iteratedTicks
@@ -31,12 +37,17 @@ if IMPORT_FROM_FILE:
 else:
   initialize_iteratedTicks()
 
+#=======================================
 # Save results
+#=======================================
 def save_results():
+  print("Saving results...")
   np.save(FILE_NAME, iteratedTicks)
   print("Results saved to %s" % FILE_NAME)
 
+#=======================================
 # Core - Incrementation
+#=======================================
 def increment_core(iterations=ITERATIONS, targets=None):
   # Variables
   global iteratedTicks
@@ -91,20 +102,87 @@ def increment_core(iterations=ITERATIONS, targets=None):
     if ticksSinceShard == max_ticks:
       reset_variables()
 
+
+#=======================================
+# Wrappers
+#=======================================
 # Calculate randomly
-#increment_core()
+def random_incrementation():
+  increment_core()
 
 # Calculate systematically
-try:
-  total_combinations_with_replacement = 0
-  for targets in itertools.combinations_with_replacement(range(1, MAX_TARGETS+1), MAX_TICKS):  # Lazy af
-    total_combinations_with_replacement += 1
-  combinations_iterator = 0
-  for targets in itertools.combinations_with_replacement(range(1, MAX_TARGETS+1), MAX_TICKS):
-    print("Iterating %s (%i/%i)" % (" ".join(str(i) for i in targets)), combinations_iterator, total_combinations_with_replacement)
-    increment_core(targets=targets)
-    combinations_iterator += 1
-  save_results()
-except KeyboardInterrupt:
-  print("\nInterrupted, saving results...\n")
-  save_results()
+def permuted_incrementation():
+  try:
+    while True:
+      
+      # Just mirroring real loop, LAZY AF
+      total_combinations_with_replacement = 0
+      for tick_count in range(1, MAX_TICKS+1):
+        for target_history in itertools.combinations_with_replacement(range(1, MAX_TARGETS+1), tick_count-1):
+          for last_target in range(1, MAX_TARGETS+1):
+            total_combinations_with_replacement += 1
+      
+      combinations_iterator = 1
+      for tick_count in range(1, MAX_TICKS+1):
+        for target_history in itertools.combinations_with_replacement(range(1, MAX_TARGETS+1), tick_count-1):
+          for last_target in range(1, MAX_TARGETS+1):
+            targets = target_history + (last_target,)
+            print("Iterating %s (%i/%i)" % (" ".join(str(i) for i in targets), combinations_iterator, total_combinations_with_replacement))
+            increment_core(targets=targets)
+            combinations_iterator += 1
+        save_results()
+      
+  except KeyboardInterrupt:
+    print("\nInterrupted\n")
+    save_results()
+
+# Fill holes in permuted values
+def fill_permuted_incrementation(iteration_aim):
+  try:
+    iteration_needed = True
+    while iteration_needed:
+      iteration_needed = False
+      for tick_count in range(MAX_TICKS, 0, -1):
+        print("Filling %i ticks" % tick_count)
+        for target_history in itertools.combinations_with_replacement(range(1, MAX_TARGETS+1), tick_count-1):
+          for last_target in range(1, MAX_TARGETS+1):
+              targets = target_history + (last_target,)
+              iteratedTick = iteratedTicks[targets + (0,) * (MAX_TICKS - tick_count)]
+              iteration_sum = iteratedTick[0] + iteratedTick[1]
+              if iteration_sum < iteration_aim:
+                iteration_needed = True
+                print("Iterating %s (currently %i iterations)" % (" ".join(str(i) for i in targets), iteration_sum))
+                increment_core(targets=targets)
+      save_results()
+  except KeyboardInterrupt:
+      print("\nInterrupted, saving results...\n")
+      save_results()
+
+# Fill all holes, necessary when there are non-permuted values in numpy array
+# Only fills values where there are iterations already
+def fill_permuted_incrementation(iteration_aim):
+  try:
+    iteration_needed = True
+    while iteration_needed:
+      iteration_needed = False
+      for tick_count in range(MAX_TICKS, 0, -1):
+        print("Filling %i ticks" % tick_count)
+        for target_history in itertools.product(range(1, MAX_TARGETS+1), repeat=tick_count-1):
+          for last_target in range(1, MAX_TARGETS+1):
+              targets = target_history + (last_target,)
+              iteratedTick = iteratedTicks[targets + (0,) * (MAX_TICKS - tick_count)]
+              iteration_sum = iteratedTick[0] + iteratedTick[1]
+              if iteration_sum > 0 and iteration_sum < iteration_aim:
+                iteration_needed = True
+                print("Iterating %s (currently %i iterations)" % (" ".join(str(i) for i in targets), iteration_sum))
+                increment_core(targets=targets)
+      save_results()
+  except KeyboardInterrupt:
+      print("\nInterrupted, saving results...\n")
+      save_results()
+      
+  
+#=======================================
+# Execution
+#=======================================
+fill_permuted_incrementation(1000)
