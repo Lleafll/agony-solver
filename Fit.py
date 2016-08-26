c49e4f169e5dbf407498eeba743039d8e4b7b7da
Lars Lorenz <lars.ll.lorenz@gmail.com>
2016-08-26 16:29:25 +0200

    Optimize fit and value handling

--- Fit.py
+++ Fit.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
+#=======================================
 # Modules
+#=======================================
 import itertools
 import math
 from math import log
+from math import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 from operator import mul
 from scipy.optimize import curve_fit
 from tkFileDialog import askopenfilename
 
+#=======================================
+# Load
+#=======================================
 # Ask file name
 filetypes = [("Numpy Files", "*.npy"), ("All", "*")]
 filepath = askopenfilename(filetypes=filetypes)
@@ -26,57 +32,76 @@ for i in range(1, iteratedTicks.shape[0]):
     total_iterations += result[0] + result[1]
 print("Total iterations: %i" % total_iterations)
 
+#=======================================
 # Core
+#=======================================
 def calculate_result(key):
     result = iteratedTicks[key]
     noGain = result[0]
     gain = result[1]
-    gainSum = noGain + gain
+    gainSum = noGain + gain    
     if gainSum > 0:
         chance = gain / gainSum
         if chance > 0:
             return chance
-    else:
-        print(key, "noGain + gain == 0")
 
+#=======================================
 # Fit
-def exp_fit(x, c, d, e, f):
-    return c*np.exp(d/x) + e + f*x
+#=======================================
+def fit_func(x, c, d, e):
+    #return c*np.exp(d/x) + e
+    return c / (1 + np.exp(d * x)) + e
 
-# Plot
+#=======================================
+# Iteration & Plot
+#=======================================
 plot_range = np.arange(1, MAX_TARGETS, 0.01)
-plt.figure()
-legend_handles = []
-for ticks_to_plot in range(1, MAX_TICKS+1):
-    x = np.array([])
-    y = np.array([])
-    print("Iterating %i ticks" % ticks_to_plot)    
-    for targets in itertools.combinations_with_replacement(range(1, MAX_TARGETS+1), ticks_to_plot):
-        target_factor = reduce(mul, targets)  # Product of all targets
-        target_factor = pow(target_factor, 1.0/ticks_to_plot)
-        targets += (0,) * (MAX_TICKS - ticks_to_plot)
+for current_targets in range(1, MAX_TARGETS+1):
+    plt.figure()
+    legend_handles = []
+    for history_targets in range(1, MAX_TICKS):  # Start at 2 ticks since chance is 0 without former ticks anyway
+        x = np.array([])
+        y = np.array([])
+        print("Fitting %i ticks" % history_targets)    
+        for targets in itertools.product(range(1, MAX_TARGETS+1), repeat=history_targets):  # Different from Increment.py but ensures that all combinations get used
+            # Target factor calculation
+            target_factor = reduce(mul, targets)  # Product of all targets
+            target_factor = pow(target_factor, 1.0/history_targets)
+                    
+##            target_factor = reduce(mul, map(sqrt, targets)) / history_targets # Product of all targets
+            
+            targets += (current_targets,)
+            targets += (0,) * (MAX_TICKS - history_targets - 1)
+            
+##            target_factor = sum(targets) / (1.0 * history_targets)
+               
+            chance = calculate_result(targets)
 
-        chance = calculate_result(targets)
-        if not chance is None:
-            x = np.append(x, target_factor)
-            y = np.append(y, chance)
-    
-    if x.any() and y.any():
-        opt, cov = curve_fit(exp_fit, x, y, maxfev=100000)
-    else:
-        opt = [0, 0, 0, 0]
-    plt.plot(x, y, "+", label=ticks_to_plot)
-    plt.plot(plot_range, exp_fit(plot_range, *opt))
-    legend_handles.append("%i Ticks, %f, %f" % (ticks_to_plot, opt[0], opt[1]))
+            if not chance is None:
+                x = np.append(x, target_factor)
+                y = np.append(y, chance)
+        
+        plt.plot(x, y, "+", label=history_targets)
+        
+        if x.any() and y.any() and x.shape[0] > 2 and y.shape[0] > 2:
+            opt, cov = curve_fit(fit_func, x, y, maxfev=100000)
+        else:
+            opt = [0, 0, 0]
+        plt.plot(plot_range, fit_func(plot_range, *opt))
+        print(opt)
 
-    print(opt)
-    
-plt.ylabel(u"Chance")
-plt.xlabel(u"Targets")
-plt.gca().set_ylim([0, 1])
-plt.legend(legend_handles)
-plt.legend(loc="upper right")
-plt.tight_layout()
+        legend_handles.append("%i Ticks" % (history_targets))
+        legend_handles.append("%i Ticks Fit" % (history_targets))
+
+    plt.title(u"%i Current Targets" % current_targets)
+    plt.ylabel(u"Chance")
+    plt.xlabel(u"Targets")
+    plt.gca().set_ylim([0, 1])
+    plt.legend(legend_handles, loc="upper right")
+    plt.tight_layout()
 plt.show()
 
+#=======================================
+# Finishing
+#=======================================
 print("Finished")
