3004942eb96f2d9d4a058a0881fe89528c847208
Lars Lorenz <lars.ll.lorenz@gmail.com>
2016-08-29 17:35:23 +0200

    Comment (statprof usage)

--- Increment_Execute.py
+++ Increment_Execute.py
@@ -1,5 +1,3 @@
-#import pyximport; pyximport.install()
-
 from Increment import fill_permuted_incrementation
 
 #==============================================================================
@@ -9,6 +7,14 @@ from Increment import fill_permuted_incrementation
 #cProfile.run("fill_permuted_incrementation(1000)", "fill_permuted_incrementation.profile")
 #import pstats
 #pstats.Stats("fill_permuted_incrementation.profile").sort_stats("time").strip_dirs().sort_stats("time").print_stats()
+
+#import statprof
+#statprof.start()
+#try:
+#    fill_permuted_incrementation(1000)
+#finally:
+#    statprof.stop()
+#    statprof.display()
 
 #==============================================================================
 # Regular Execution
