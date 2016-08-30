df2c85c4a43b6cc21c361afe5a89b6d08fa5d1a8
Lars Lorenz <lars.ll.lorenz@gmail.com>
2016-08-30 11:03:16 +0200

    Fix bug where current target wasn't incorporated into increment chance, further performance improvements

--- Increment.pyx
+++ Increment.pyx
@@ -60,8 +60,10 @@ def save_results():
     if DEBUG:
         return
     print("Saving results...")
+    
     with open(FILE_NAME, "wb") as f:
         pickle.dump(iteratedTicks, f)
+    
     print("Results saved to %s" % FILE_NAME)
 
 #==============================================================================
@@ -70,8 +72,10 @@ def save_results():
 cdef void addTickResult(int* failure, int* success, int targetMax, tuple targets):
     global iteratedTicks
     cdef int targetIndex
-    for targetIndex in xrange(0, targetMax):
+    
+    for targetIndex in range(0, targetMax):
         key = targets[0:targetIndex+1]
+        
         try:
             iteratedTicks[key][0] += failure[targetIndex]
             iteratedTicks[key][1] += success[targetIndex]
@@ -85,10 +89,20 @@ cdef void addTickResult(int* failure, int* success, int targetMax, tuple targets
 cdef inline float rng_reset():
     return rand() * RESET_MAX / RAND_MAX - RESET_MIN
 
-@cython.profile(False)
+cdef float squareRootsFactor[100]  # Used to store pre-calculated square roots, indices corresponding to indices in targets tuple
 @cython.cdivision(True)
-cdef inline float rng_increment():
-    return rand() * INCREMENT_MAX / RAND_MAX - INCREMENT_MIN
+cdef void pre_calculate_square_roots_factor(tuple targets):
+    global squareRootsFactor
+    cdef int target
+    cdef int targetIndex = 0
+    
+    for target in targets:
+        squareRootsFactor[targetIndex] = INCREMENT_MAX / RAND_MAX * target**0.5
+        targetIndex += 1
+
+@cython.profile(False)
+cdef inline float rng_increment(int targetIndex):
+    return rand() * squareRootsFactor[targetIndex]
 
 cdef void accumulate_core(int* failure, int* success, int targetMax, int iterationSets):
     cdef:
@@ -97,10 +111,12 @@ cdef void accumulate_core(int* failure, int* success, int targetMax, int iterati
         int currentTargets
         int targetIndex
     
-    for iterationCounter in xrange(0, iterationSets):
+    for iterationCounter in range(0, iterationSets):
         accumulator = rng_reset()
-        for targetIndex in xrange(0, targetMax):
-            accumulator += rng_increment()
+        
+        for targetIndex in range(0, targetMax):
+            accumulator += rng_increment(targetIndex)
+            
             if accumulator > 1:
                 success[targetIndex] += 1
                 break
@@ -113,7 +129,10 @@ cdef void increment_core(tuple targets, int iterationSets):
         int failure[100]
         int success[100]
         int resultIndex
-    for resultIndex in xrange(0, targetMax):
+    
+    pre_calculate_square_roots_factor(targets)
+    
+    for resultIndex in range(0, targetMax):
         failure[resultIndex] = 0
         success[resultIndex] = 0
     
@@ -127,34 +146,43 @@ cdef void increment_core(tuple targets, int iterationSets):
 cdef tuple targets
 def fill_permuted_incrementation(iteration_aim):
     try:
-        iteration_needed = True
-        iterations_total = 0
+        further_iteration_needed = True
         iterations_save = 0
-        while iteration_needed:
-            iteration_needed = False
+        
+        while further_iteration_needed:
+            iterations_total = float("inf")
+            further_iteration_needed = False
+            
             for tick_count in range(MAX_TICKS, 0, -1):
                 print("Filling %i ticks" % tick_count)
+                
                 for target_history in itertools.combinations_with_replacement(range(MIN_TARGETS, MAX_TARGETS+1), tick_count-1):
+                    
                     for last_target in range(MIN_TARGETS, MAX_TARGETS+1):
                         targets = target_history + (last_target,)
+                        
                         try:
                             iteratedTick = iteratedTicks[targets]
                             iteration_sum = iteratedTick[0] + iteratedTick[1]
                         except KeyError:
                             iteration_sum = 0
+                        
                         if iteration_sum < iteration_aim:
-                            iteration_needed = True
+                            further_iteration_needed = True
                             increment_core(targets, ITERATIONS)
                             iterations_total += ITERATIONS
+                            
                             if PRINT_OUTPUT and iterations_total > PRINT_OUTPUT_EVERY:
                                 print("Iterating %s (currently %i iterations)" % (" ".join(str(i) for i in targets), iteration_sum))
                                 iterations_total = 0
                             iterations_save += ITERATIONS
+                            
                             if iterations_save > SAVE_EVERY:
                                 save_results()
                                 iterations_save = 0
 
             save_results()
+            
     except KeyboardInterrupt:
         print("\nInterrupted, saving results...\n")
         save_results()
